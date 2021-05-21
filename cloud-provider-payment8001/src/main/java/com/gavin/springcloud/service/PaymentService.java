f792da4b78e67f38a0e633a1fadc677d8aa9649e
Gavin Birkhoff <gavinbirkhoff@gmail.com>
2021-05-21 22:02:51 +0800

    cloud payment order 模块添加

---
+++ cloud-provider-payment8001/src/main/java/com/gavin/springcloud/service/PaymentService.java
@@ -0,0 +1,11 @@
+package com.gavin.springcloud.service;
+
+import com.gavin.springcloud.entities.Payment;
+import org.apache.ibatis.annotations.Param;
+
+public interface PaymentService
+{
+    public int create(Payment payment);
+
+    public Payment getPaymentById(@Param("id") Long id);
+}
