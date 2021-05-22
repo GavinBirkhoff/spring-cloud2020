d71115fe1aa131d8b4f86691988eb70e809da5e7
Gavin Birkhoff <gavinbirkhoff@gmail.com>
2021-05-22 20:00:03 +0800

    Eureka 单机 集群 actuator服务信息 服务发现discovery

--- cloud-provider-payment8002/src/main/java/com/gavin/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8002/src/main/java/com/gavin/springcloud/controller/PaymentController.java
@@ -5,24 +5,18 @@ import com.gavin.springcloud.entities.Payment;
 import com.gavin.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 @RestController
 @Slf4j
-public class PaymentController {
+public class PaymentController
+{
     @Resource
     private PaymentService paymentService;
-
     @Value("${server.port}")
     private String serverPort;
-
-    @Resource
-    private DiscoveryClient discoveryClient;
 
     @PostMapping(value = "/payment/create")
     public CommonResult create(@RequestBody Payment payment)
@@ -51,19 +45,9 @@ public class PaymentController {
         }
     }
 
-    @GetMapping(value = "/payment/discovery")
-    public Object discovery()
+    @GetMapping(value = "/payment/lb")
+    public String getPaymentLB()
     {
-        List<String> services = discoveryClient.getServices();
-        for (String element : services) {
-            log.info("*****element: "+element);
-        }
-
-        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
-        for (ServiceInstance instance : instances) {
-            log.info(instance.getServiceId()+"\t"+instance.getHost()+"\t"+instance.getPort()+"\t"+instance.getUri());
-        }
-
-        return this.discoveryClient;
+        return serverPort;
     }
 }
