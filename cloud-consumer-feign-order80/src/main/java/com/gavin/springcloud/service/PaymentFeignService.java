ddd34e4ea6a1bcf2d40f086feec656519d60808d
Gavin Birkhoff <gavinbirkhoff@gmail.com>
2021-05-23 20:25:02 +0800

    OpenFeign服务调用 超时控制 日志增强

---
+++ cloud-consumer-feign-order80/src/main/java/com/gavin/springcloud/service/PaymentFeignService.java
@@ -0,0 +1,19 @@
+package com.gavin.springcloud.service;
+
+import com.gavin.springcloud.entities.CommonResult;
+import com.gavin.springcloud.entities.Payment;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Component
+@FeignClient(value = "CLOUD-PAYMENT-SERVICE")
+public interface PaymentFeignService
+{
+    @GetMapping(value = "/payment/get/{id}")
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id);
+
+    @GetMapping(value = "/payment/feign/timeout")
+    public String paymentFeignTimeout();
+}
